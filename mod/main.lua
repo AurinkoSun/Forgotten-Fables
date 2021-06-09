35b54cb2b8a95c875021f3e05c7d029e1c5d74c1
Kyojo <sethfb@gmail.com>
2021-06-09 15:08:18 -0400

    Added Brim and Sticky synergies, fixed flying bomb bug

--- mod/main.lua
+++ mod/main.lua
@@ -39,35 +39,133 @@ function onFireTear(self, tear)
     if player == nil then
         return
     end
-    if player:HasCollectible(fatFetusID) then
-        local dmg = player.Damage * (300 / 3.5)
-        bomb = Isaac.Spawn(EntityType.ENTITY_BOMBDROP, 17, 0, tear.Position, tear.Velocity, player):ToBomb()
-        tear:Remove()
-        if bomb ~= nil then
-            bomb.IsFetus = true
+    if player:HasCollectible(CollectibleType.COLLECTIBLE_SATURNUS) then
+    else
+        if player:HasCollectible(fatFetusID) then
+            local dmg = player.Damage * (300 / 3.5)
+            bomb = Isaac.Spawn(EntityType.ENTITY_BOMBDROP, 21, 0, player.Position, tear.Velocity, player):ToBomb()
+            if bomb == nil then
+                return
+            end
+            bomb:GetData().colupdated = false
+            tear:Remove()
             bomb.ExplosionDamage = dmg
-            if player:HasCollectible(CollectibleType.COLLECTIBLE_BRIMSTONE_BOMBS) or player:HasCollectible(CollectibleType.COLLECTIBLE_BRIMSTONE) then
-                bomb:AddTearFlags(TearFlags.TEAR_BRIMSTONE_BOMB)
-            end
-            if player:HasCollectible(CollectibleType.COLLECTIBLE_BUTT_BOMBS) then
-                bomb:AddTearFlags(TearFlags.TEAR_BUTT_BOMB)
-            end
-            if player:HasCollectible(CollectibleType.COLLECTIBLE_BOMBER_BOY) then
-                bomb:AddTearFlags(TearFlags.TEAR_CROSS_BOMB)
-            end
-            if player:HasCollectible(CollectibleType.COLLECTIBLE_GHOST_BOMBS) then
-                bomb:AddTearFlags(TearFlags.TEAR_GHOST_BOMB)
-            end
-            if player:HasCollectible(CollectibleType.COLLECTIBLE_BLOOD_BOMBS) then
-                bomb:AddTearFlags(TearFlags.TEAR_BLOOD_BOMB)
-            end
-            if player:HasCollectible(CollectibleType.COLLECTIBLE_SCATTER_BOMBS) or player:HasCollectible(CollectibleType.COLLECTIBLE_SAD_BOMBS) then
-                bomb:AddTearFlags(TearFlags.TEAR_SCATTER_BOMB)
-            end
         end
     end
 end
 FatFetus:AddCallback(ModCallbacks.MC_POST_FIRE_TEAR, onFireTear)
+BombTypes = BombTypes or ({})
+BombTypes.Normal = 0
+BombTypes[BombTypes.Normal] = "Normal"
+BombTypes.Brim = 1
+BombTypes[BombTypes.Brim] = "Brim"
+BombTypes.Sticky = 2
+BombTypes[BombTypes.Sticky] = "Sticky"
+BombTypes.Cross = 3
+BombTypes[BombTypes.Cross] = "Cross"
+BombTypes.Blood = 4
+BombTypes[BombTypes.Blood] = "Blood"
+BombTypes.Mega = 5
+BombTypes[BombTypes.Mega] = "Mega"
+BombTypes.Glitter = 6
+BombTypes[BombTypes.Glitter] = "Glitter"
+BombTypes.Random = 7
+BombTypes[BombTypes.Random] = "Random"
+function fetusGigaUpdate(self, bomb)
+    bomb.CollisionDamage = 0
+    if bomb.SpawnerEntity ~= nil then
+        local player = bomb.SpawnerEntity:ToPlayer()
+        if player ~= nil then
+            if bomb:GetData().colupdated == false then
+                bomb:SetExplosionCountdown(100)
+                print("colupdated=false")
+                if player:HasCollectible(CollectibleType.COLLECTIBLE_BRIMSTONE_BOMBS) or player:HasCollectible(CollectibleType.COLLECTIBLE_BRIMSTONE) then
+                    bomb:GetData().colupdated = true
+                    bomb:GetData().syntype = BombTypes.Brim
+                    bomb:GetSprite():Play("brimpulse", true)
+                elseif player:HasCollectible(CollectibleType.COLLECTIBLE_STICKY_BOMBS) then
+                    bomb:GetData().colupdated = true
+                    bomb:GetData().syntype = BombTypes.Sticky
+                    bomb:AddTearFlags(TearFlags.TEAR_STICKY)
+                    bomb:GetSprite():Play("stickypulse", true)
+                else
+                    bomb:GetData().colupdated = true
+                    bomb:GetData().syntype = BombTypes.Normal
+                    bomb:GetSprite():Play("Pulse", true)
+                end
+            end
+            if (bomb:GetSprite():IsFinished("Pulse") or bomb:GetSprite():IsFinished("brimpulse")) or bomb:GetSprite():IsFinished("stickypulse") then
+                if bomb:GetData().syntype == BombTypes.Brim then
+                    EntityLaser.ShootAngle(
+                        1,
+                        bomb.Position,
+                        0,
+                        15,
+                        Vector(0, 0),
+                        bomb
+                    )
+                    EntityLaser.ShootAngle(
+                        1,
+                        bomb.Position,
+                        90,
+                        15,
+                        Vector(0, 0),
+                        bomb
+                    )
+                    EntityLaser.ShootAngle(
+                        1,
+                        bomb.Position,
+                        180,
+                        15,
+                        Vector(0, 0),
+                        bomb
+                    )
+                    EntityLaser.ShootAngle(
+                        1,
+                        bomb.Position,
+                        270,
+                        15,
+                        Vector(0, 0),
+                        bomb
+                    )
+                end
+                local explody = Isaac.Spawn(
+                    4,
+                    17,
+                    0,
+                    bomb.Position,
+                    Vector(0, 0),
+                    player
+                ):ToBomb()
+                if explody == nil then
+                    print("broken af")
+                    return
+                end
+                explody.Visible = false
+                if bomb:GetData().syntype == BombTypes.Sticky then
+                    explody:AddTearFlags(TearFlags.TEAR_STICKY)
+                end
+                explody:SetExplosionCountdown(0)
+                bomb:GetSprite():Play("Explode", false)
+            end
+            if bomb:GetSprite():IsFinished("Explode") then
+                bomb:Remove()
+            end
+        else
+            if bomb:GetData().colupdated == false then
+                bomb:SetExplosionCountdown(87)
+                bomb.ExplosionDamage = 300
+                bomb:GetData().colupdated = true
+                bomb:GetData().syntype = BombTypes.Normal
+                bomb:GetSprite():Play("Pulse", true)
+            end
+            if bomb:GetSprite():IsFinished("Pulse") then
+                bomb:GetSprite():Play("Explode", false)
+            end
+        end
+    end
+end
+FatFetus:AddCallback(ModCallbacks.MC_POST_BOMB_UPDATE, fetusGigaUpdate, 21)
 end,
 }
 return require("main")
--- src/main.ts
+++ src/main.ts
@@ -19,32 +19,85 @@ function onFireTear(tear:EntityTear){
   if(player === null){
     return;
   }
-  if(player.HasCollectible(fatFetusID)){
-  let dmg = player.Damage *(300/3.5);
-  bomb=Isaac.Spawn(EntityType.ENTITY_BOMBDROP, 17, 0, tear.Position, tear.Velocity, player).ToBomb();
-  tear.Remove();
-    if(bomb!=null){
-      bomb.IsFetus=true;
+  if(player.HasCollectible(CollectibleType.COLLECTIBLE_SATURNUS)){
+  } else {
+    if(player.HasCollectible(fatFetusID)){
+      let dmg = player.Damage *(300/3.5);
+      bomb=Isaac.Spawn(EntityType.ENTITY_BOMBDROP, 21, 0, player.Position, tear.Velocity, player).ToBomb();
+      if(bomb==null){
+        return;
+      }
+      bomb.GetData().colupdated=false;
+      tear.Remove();
       bomb.ExplosionDamage=dmg;
-      if(player.HasCollectible(CollectibleType.COLLECTIBLE_BRIMSTONE_BOMBS) || player.HasCollectible(CollectibleType.COLLECTIBLE_BRIMSTONE)){
-        bomb.AddTearFlags(TearFlags.TEAR_BRIMSTONE_BOMB);
-      }
-      if(player.HasCollectible(CollectibleType.COLLECTIBLE_BUTT_BOMBS)){
-        bomb.AddTearFlags(TearFlags.TEAR_BUTT_BOMB);
-      }
-      if (player.HasCollectible(CollectibleType.COLLECTIBLE_BOMBER_BOY)){
-        bomb.AddTearFlags(TearFlags.TEAR_CROSS_BOMB);
-      }
-      if (player.HasCollectible(CollectibleType.COLLECTIBLE_GHOST_BOMBS)){
-        bomb.AddTearFlags(TearFlags.TEAR_GHOST_BOMB);
-      }
-      if(player.HasCollectible(CollectibleType.COLLECTIBLE_BLOOD_BOMBS)){
-        bomb.AddTearFlags(TearFlags.TEAR_BLOOD_BOMB);
-      }
-      if(player.HasCollectible(CollectibleType.COLLECTIBLE_SCATTER_BOMBS) || player.HasCollectible(CollectibleType.COLLECTIBLE_SAD_BOMBS)){
-        bomb.AddTearFlags(TearFlags.TEAR_SCATTER_BOMB);
-      }
     }
   }
 }
 FatFetus.AddCallback(ModCallbacks.MC_POST_FIRE_TEAR,onFireTear);
+enum BombTypes{
+  Normal,
+  Brim,
+  Sticky,
+  Cross,
+  Blood,
+  Mega,
+  Glitter,
+  Random
+}
+function fetusGigaUpdate(bomb:EntityBomb){
+  bomb.CollisionDamage=0;
+  if(bomb.SpawnerEntity!=null){
+    let player = bomb.SpawnerEntity.ToPlayer();
+    if(player!=null){
+      if(bomb.GetData().colupdated==false){
+        bomb.SetExplosionCountdown(100);
+        print("colupdated=false");
+        if(player.HasCollectible(CollectibleType.COLLECTIBLE_BRIMSTONE_BOMBS) || player.HasCollectible(CollectibleType.COLLECTIBLE_BRIMSTONE)){
+          bomb.GetData().colupdated=true;
+          bomb.GetData().syntype=BombTypes.Brim;
+          bomb.GetSprite().Play("brimpulse", true);
+        } else if(player.HasCollectible(CollectibleType.COLLECTIBLE_STICKY_BOMBS)){
+          bomb.GetData().colupdated=true;
+          bomb.GetData().syntype=BombTypes.Sticky;
+          bomb.AddTearFlags(TearFlags.TEAR_STICKY);
+          bomb.GetSprite().Play("stickypulse", true);
+        } else {
+          bomb.GetData().colupdated=true;
+          bomb.GetData().syntype=BombTypes.Normal;
+          bomb.GetSprite().Play("Pulse", true);
+        }
+      }
+      if(bomb.GetSprite().IsFinished("Pulse") || bomb.GetSprite().IsFinished("brimpulse") || bomb.GetSprite().IsFinished("stickypulse")){
+        if(bomb.GetData().syntype===BombTypes.Brim){
+          EntityLaser.ShootAngle(LaserVariant.THICK_RED,bomb.Position,0,15,Vector(0,0),bomb);
+          EntityLaser.ShootAngle(LaserVariant.THICK_RED,bomb.Position,90,15,Vector(0,0),bomb);
+          EntityLaser.ShootAngle(LaserVariant.THICK_RED,bomb.Position,180,15,Vector(0,0),bomb);
+          EntityLaser.ShootAngle(LaserVariant.THICK_RED,bomb.Position,270,15,Vector(0,0),bomb);
+        }
+        let explody=Isaac.Spawn(4, 17, 0, bomb.Position, Vector(0,0), player).ToBomb();
+        if(explody===null){print("broken af"); return;}
+        explody.Visible=false;
+        if(bomb.GetData().syntype===BombTypes.Sticky){
+          explody.AddTearFlags(TearFlags.TEAR_STICKY);
+        }
+        explody.SetExplosionCountdown(0);
+        bomb.GetSprite().Play("Explode", false);
+      }
+      if(bomb.GetSprite().IsFinished("Explode")){
+        bomb.Remove();
+      }
+    } else {
+      if(bomb.GetData().colupdated==false){
+        bomb.SetExplosionCountdown(87);
+        bomb.ExplosionDamage=300;
+        bomb.GetData().colupdated=true;
+        bomb.GetData().syntype=BombTypes.Normal;
+        bomb.GetSprite().Play("Pulse",true);
+      }
+      if(bomb.GetSprite().IsFinished("Pulse")){
+        bomb.GetSprite().Play("Explode",false);
+      }
+    }
+  }
+}
+FatFetus.AddCallback(ModCallbacks.MC_POST_BOMB_UPDATE,fetusGigaUpdate,21);
