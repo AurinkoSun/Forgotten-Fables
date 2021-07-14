ff5d8a570bac0e4a42c3d2ea5796a34e727227a7
Kyojo <sethfb@gmail.com>
2021-07-14 18:50:35 -0400

    Fixed blue heart

--- src/items/passive/blueheart.ts
+++ src/items/passive/blueheart.ts
@@ -1,23 +1,39 @@
 import { game, ModItemTypes } from "../../constants";
 
 export function blueHeart(pos: Vector): void {
-  let numBlueHearts = 0;
-  for (let i = 0; i < game.GetNumPlayers(); i++) {
-    const player = game.GetPlayer(i);
-    if (player !== null && player.HasCollectible(ModItemTypes.BLUE_HEART)) {
-      numBlueHearts += 1;
-    }
-  }
   if (Game().GetRoom().GetType() === RoomType.ROOM_BOSS) {
-    for (let i = 0; i < numBlueHearts * 3; i++) {
-      Isaac.Spawn(
-        EntityType.ENTITY_PICKUP,
-        PickupVariant.PICKUP_HEART,
-        HeartSubType.HEART_SOUL,
-        Game().GetRoom().FindFreePickupSpawnPosition(pos),
-        Vector(0, 0),
-        null,
-      );
+    for (let i = 0; i < game.GetNumPlayers(); i++) {
+      const player = game.GetPlayer(i);
+      if (
+        player !== null &&
+        player.HasCollectible(ModItemTypes.BLUE_HEART) &&
+        Game().GetRoom().GetType() === RoomType.ROOM_BOSS
+      ) {
+        Isaac.Spawn(
+          EntityType.ENTITY_PICKUP,
+          PickupVariant.PICKUP_HEART,
+          HeartSubType.HEART_SOUL,
+          Game().GetRoom().FindFreePickupSpawnPosition(pos),
+          Vector(0, 0),
+          null,
+        );
+        Isaac.Spawn(
+          EntityType.ENTITY_PICKUP,
+          PickupVariant.PICKUP_HEART,
+          HeartSubType.HEART_SOUL,
+          Game().GetRoom().FindFreePickupSpawnPosition(pos),
+          Vector(0, 0),
+          null,
+        );
+        Isaac.Spawn(
+          EntityType.ENTITY_PICKUP,
+          PickupVariant.PICKUP_HEART,
+          HeartSubType.HEART_SOUL,
+          Game().GetRoom().FindFreePickupSpawnPosition(pos),
+          Vector(0, 0),
+          null,
+        );
+      }
     }
   }
 }
