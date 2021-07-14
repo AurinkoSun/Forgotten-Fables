217f679fb888f25ecd8b433c610bf85c713e128d
Kyojo <sethfb@gmail.com>
2021-07-14 19:13:23 -0400

    Fixed russian roulette

--- mod/items/active/russianroulette.lua
+++ mod/items/active/russianroulette.lua
@@ -3,14 +3,14 @@ local ____exports = {}
 local ____constants = require("constants")
 local sfxManager = ____constants.sfxManager
 function ____exports.rRoulette(self, rng, player)
-    if rng:RandomInt(6) == 6 then
+    if rng:RandomInt(6) == 3 then
         player:TakeDamage(
             255,
             DamageFlag.DAMAGE_NO_MODIFIERS,
             EntityRef(player),
             0
         )
-        sfxManager:Play(SoundEffect.SOUND_EXPLOSION_STRONG)
+        sfxManager:Play(SoundEffect.SOUND_EXPLOSION_WEAK)
     else
         for ____, entity in ipairs(
             Isaac.GetRoomEntities()
@@ -19,7 +19,7 @@ function ____exports.rRoulette(self, rng, player)
                 entity:Kill()
             end
         end
-        sfxManager:Play(SoundEffect.SOUND_EXPLOSION_WEAK)
+        sfxManager:Play(SoundEffect.SOUND_EXPLOSION_STRONG)
     end
     return true
 end
--- src/items/active/russianroulette.ts
+++ src/items/active/russianroulette.ts
@@ -1,21 +1,21 @@
 import { sfxManager } from "../../constants";
 
 export function rRoulette(rng: RNG, player: EntityPlayer): boolean {
-  if (rng.RandomInt(6) === 6) {
+  if (rng.RandomInt(6) === 3) {
     player.TakeDamage(
       255,
       DamageFlag.DAMAGE_NO_MODIFIERS,
       EntityRef(player),
       0,
     );
-    sfxManager.Play(SoundEffect.SOUND_EXPLOSION_STRONG);
+    sfxManager.Play(SoundEffect.SOUND_EXPLOSION_WEAK);
   } else {
     for (const entity of Isaac.GetRoomEntities()) {
       if (entity.IsVulnerableEnemy()) {
         entity.Kill();
       }
     }
-    sfxManager.Play(SoundEffect.SOUND_EXPLOSION_WEAK);
+    sfxManager.Play(SoundEffect.SOUND_EXPLOSION_STRONG);
   }
   return true;
 }
