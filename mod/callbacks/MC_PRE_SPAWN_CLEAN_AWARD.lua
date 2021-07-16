8ecf0c224dc3cf75e08044140aaab1de10732bb7
Kyojo <sethfb@gmail.com>
2021-07-16 08:02:13 -0400

    Fixed issue #17

--- mod/callbacks/MC_PRE_SPAWN_CLEAN_AWARD.lua
+++
@@ -1,8 +0,0 @@
---[[ Generated with https://github.com/TypeScriptToLua/TypeScriptToLua ]]
-local ____exports = {}
-local ____blueheart = require("items.passive.blueheart")
-local blueHeart = ____blueheart.blueHeart
-function ____exports.roomClearAward(self, _rng, pos)
-    blueHeart(nil, pos)
-end
-return ____exports
