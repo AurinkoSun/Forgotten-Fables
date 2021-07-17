e292ff9ef5a05c072547dad154126d60cbfa1204
Kyojo <sethfb@gmail.com>
2021-07-17 18:10:30 -0400

    removed pre-bundle lua remnants

--- mod/callbacks/MC_NPC_UPDATE.lua
+++
@@ -1,8 +0,0 @@
---[[ Generated with https://github.com/TypeScriptToLua/TypeScriptToLua ]]
-local ____exports = {}
-local ____peel = require("globals.peel")
-local peelUpdate = ____peel.peelUpdate
-function ____exports.npcUpdate(self, entity)
-    peelUpdate(nil, entity)
-end
-return ____exports
