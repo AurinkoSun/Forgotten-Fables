c82361f3666a8d989f0cdbddac1b272d867f49e5
Kyojo <sethfb@gmail.com>
2021-07-20 14:56:43 -0400

    Updated EID descriptions

--- mod/main.lua
+++ mod/main.lua
@@ -2466,13 +2466,13 @@ function ____exports.alabasterStats(self, player, flags, modPlayerData)
         end
     end
 end
-local stats = {0.15, 0.15, 0.15, 0.15, 0.15}
+____exports.stats = {0.15, 0.15, 0.15, 0.15, 0.15}
 local flags = {CacheFlag.CACHE_DAMAGE, CacheFlag.CACHE_FIREDELAY, CacheFlag.CACHE_RANGE, CacheFlag.CACHE_SPEED, CacheFlag.CACHE_SHOTSPEED}
 function ____exports.birthright(self, player, modPlayerData)
     if player:HasCollectible(CollectibleType.COLLECTIBLE_BIRTHRIGHT) and (player:GetPlayerType() == ModPlayerTypes.ALABASTER) then
         local rand = rng:RandomInt(6)
         local ____obj, ____index = modPlayerData.data[GetPlayerId(nil, player) + 1].tStats, rand + 1
-        ____obj[____index] = ____obj[____index] + stats[rand + 1]
+        ____obj[____index] = ____obj[____index] + ____exports.stats[rand + 1]
         player:AddCacheFlags(flags[rand + 1])
         player:EvaluateItems()
     end
@@ -3615,21 +3615,19 @@ return ____exports
  end,
 ["globals.EID"] = function() --[[ Generated with https://github.com/TypeScriptToLua/TypeScriptToLua ]]
 local ____exports = {}
+local ____constants = require("constants")
+local ModItemTypes = ____constants.ModItemTypes
+local ModPlayerTypes = ____constants.ModPlayerTypes
 function ____exports.descriptions(self)
     if EID ~= nil then
         Isaac.DebugString("adding eid stuff")
-        EID:addCollectible(
-            Isaac.GetItemIdByName("Ghost Shot"),
-            "Turns your tears into ghosts#When they touch an enemy, ghosts explode,#dealing 0.4x your damage 3 times",
-            "Ghost Shot",
-            "en_us"
-        )
-        EID:addBirthright(
-            Isaac.GetPlayerTypeByName("Alabaster", false),
-            "Alabaster now gets small#random stat ups similar#to Candy Heart when#consuming hearts with#Blood Drive",
-            "Alabaster",
-            "en_us"
-        )
+        EID:addCollectible(ModItemTypes.GHOST_SHOT, "Turns your tears into ghosts that explode", "Ghost Shot", "en_us")
+        EID:addCollectible(ModItemTypes.GHOST_SHOT, "Rerolls items into the item with the 'opposite' id. With no mods, item 1 (sad onion) rolls into item 729 (Decap Attack)", "Stone D6", "en_us")
+        EID:addCollectible(ModItemTypes.BLUE_HEART, "Gives 3 soul hearts.", "Blue Heart", "en_us")
+        EID:addCollectible(ModItemTypes.GHOST_SHOT, "Turns your tears into ghosts#When they touch an enemy or the ground, ghosts explode,dealing 0.4x your damage 3 times.", "Ghost Shot", "en_us_detailed")
+        EID:addCollectible(ModItemTypes.BLOODDRIVE, "Alabaster's pocket active. Turns 2{{Heart}} into random items if used in a Devil or Angel room. Chances are as follows:#27% chance for a half soul heart(Angel Room) or black heart (Devil Room)#15% for a bomb#15% for a key#15% for 3 coins#10% for card or pill#8% for a trinket#5% for Rune or soul stone#4% for a  random chest (red, gold, grey, old, wooden)#1% chance for a random item", "Blood Drive", "en_us_detailed")
+        EID:addCollectible(ModItemTypes.BLOODDRIVE, "Alabaster's pocket active. Turns 2{{Heart}} into one pickup if used in a Devil or Angel room. Can give items.", "Blood Drive", "en_us_detailed")
+        EID:addBirthright(ModPlayerTypes.ALABASTER, "Alabaster now gets small random stat ups similar to Candy Heart when consuming {{Heart}}hearts with Blood Drive", "Alabaster", "en_us")
     end
 end
 return ____exports
--- src/globals/EID.ts
+++ src/globals/EID.ts
@@ -1,15 +1,47 @@
+import { ModItemTypes, ModPlayerTypes } from "../constants";
+
 export function descriptions(): void {
   if (EID !== null) {
     Isaac.DebugString("adding eid stuff");
     EID.addCollectible(
-      Isaac.GetItemIdByName("Ghost Shot"),
-      "Turns your tears into ghosts#When they touch an enemy, ghosts explode,#dealing 0.4x your damage 3 times",
+      ModItemTypes.GHOST_SHOT,
+      "Turns your tears into ghosts that explode",
       "Ghost Shot",
       "en_us",
     );
+    EID.addCollectible(
+      ModItemTypes.GHOST_SHOT,
+      "Rerolls items into the item with the 'opposite' id. With no mods, item 1 (sad onion) rolls into item 729 (Decap Attack)",
+      "Stone D6",
+      "en_us",
+    );
+    EID.addCollectible(
+      ModItemTypes.BLUE_HEART,
+      "Gives 3 soul hearts.",
+      "Blue Heart",
+      "en_us",
+    );
+    EID.addCollectible(
+      ModItemTypes.GHOST_SHOT,
+      "Turns your tears into ghosts#When they touch an enemy or the ground, ghosts explode,dealing 0.4x your damage 3 times.",
+      "Ghost Shot",
+      "en_us_detailed",
+    );
+    EID.addCollectible(
+      ModItemTypes.BLOODDRIVE,
+      "Alabaster's pocket active. Turns 2{{Heart}} into random items if used in a Devil or Angel room. Chances are as follows:#27% chance for a half soul heart(Angel Room) or black heart (Devil Room)#15% for a bomb#15% for a key#15% for 3 coins#10% for card or pill#8% for a trinket#5% for Rune or soul stone#4% for a  random chest (red, gold, grey, old, wooden)#1% chance for a random item",
+      "Blood Drive",
+      "en_us_detailed",
+    );
+    EID.addCollectible(
+      ModItemTypes.BLOODDRIVE,
+      "Alabaster's pocket active. Turns 2{{Heart}} into one pickup if used in a Devil or Angel room. Can give items.",
+      "Blood Drive",
+      "en_us_detailed",
+    );
     EID.addBirthright(
-      Isaac.GetPlayerTypeByName("Alabaster", false),
-      "Alabaster now gets small#random stat ups similar#to Candy Heart when#consuming hearts with#Blood Drive",
+      ModPlayerTypes.ALABASTER,
+      "Alabaster now gets small random stat ups similar to Candy Heart when consuming {{Heart}}hearts with Blood Drive",
       "Alabaster",
       "en_us",
     );
