204604f28d63bb7019c3bb422dc4e2852dfb1ff6
Kyojo <sethfb@gmail.com>
2021-07-20 16:47:39 -0400

    Added EID descriptions for Meat bucket and Bomb Converter

--- src/globals/EID.ts
+++ src/globals/EID.ts
@@ -28,6 +28,18 @@ export function descriptions(): void {
       "en_us_detailed",
     );
     EID.addCollectible(
+      ModItemTypes.MEATBUCKET,
+      "On use, sucks up all red hearts from the ground and gives you one blood clot per full heart",
+      "Bucket of Meat",
+      "en_us",
+    );
+    EID.addCollectible(
+      ModItemTypes.BOMBCONVERTER,
+      "On use, converts 15 bombs into 1 giga bomb.",
+      "Bomb Converter",
+      "en_us",
+    );
+    EID.addCollectible(
       ModItemTypes.BLOODDRIVE,
       "Alabaster's pocket active. Turns 2{{Heart}} into random items if used in a Devil or Angel room. Chances are as follows:#27% chance for a half soul heart(Angel Room) or black heart (Devil Room)#15% for a bomb#15% for a key#15% for 3 coins#10% for card or pill#8% for a trinket#5% for Rune or soul stone#4% for a  random chest (red, gold, grey, old, wooden)#1% chance for a random item",
       "Blood Drive",
